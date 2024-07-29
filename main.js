7b35e7031ed1e18dbc4e06753ef97b4c8234784c
User-Takhirov <tohirovfayruz@gmail.com>
2024-07-29 20:29:49 +0500

    finished

---
+++ main.js
@@ -0,0 +1,8 @@
+let header_btn = document.querySelector(".header_btn");
+let body = document.querySelector("body");
+
+header_btn.addEventListener("click", () => {
+  body.classList.toggle("dark");
+});
+// dark mode
+
